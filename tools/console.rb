20e6b4fc9fa974d2a30f59bf2425b1c119ca1749
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-11-20 17:41:43 -0800

    Creates many to many relationship between VC,Startup, and Funding classes

--- tools/console.rb
+++ tools/console.rb
@@ -7,22 +7,26 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
-mitch = Startup.new("tech", "www", "Mitch")
-juan = Startup.new("tech2", "www", "Juan")
-juan2 = Startup.new("tech2.1", "www", "Juan") 
+mitch = Startup.new("tech", "www.tech", "Mitch")
+juan = Startup.new("tech2", "www.tech2", "Juan")
+juan2 = Startup.new("tech2.1", "www.tech2.1", "Juan") 
 
 v1 = VentureCapitalist.new("Ix")
 v2 = VentureCapitalist.new("Adam")
 v3 = VentureCapitalist.new("Nick")
 
-f1 = FundingRound.new("seed",100000)
-f2 = FundingRound.new("seed", 100000)
+f1 = FundingRound.new("seed",100000,"tech", "Ix")
+
 
 v1.total_worth = 1000000000
 v2.total_worth = 1000000
 v3.total_worth = 10000000000
 
-mitch.sign_contract("Angel",10)
+mitch.sign_contract("Angel",10, "Adam")
+mitch.sign_contract("Angle",30,"Ix")
+mitch.sign_contract("Series A",10000,"Nick")
+
+v1.offer_contract("Angel",10000,"tech2")
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
